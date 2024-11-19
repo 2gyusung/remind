bbc1ac04cc1514685de4f66dfdbcb608730f33a9
dev293 <leegs005@naver.com>
2024-11-20 06:55:18 +0900

    feat : 업로드

---
+++ ToDo-list/script.js
@@ -0,0 +1,8 @@
+const keyCodeCheck = function () {
+  if (window.event.keyCode === 13);
+  {
+   const liEl =  document.createElement('li')
+   const spanEl = document.createElement('span')
+   
+  }
+};
